4eb384ac7c37f8072fd1bf81f1b3d83cf5afd259
BrunoJusta <brunomajusta@gmail.com>
2020-12-27 20:54:28 +0000

    google start

--- app/src/main/java/com/example/fcm_tour/Login2.java
+++ app/src/main/java/com/example/fcm_tour/Login2.java
@@ -3,9 +3,11 @@ package com.example.fcm_tour;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Login2 extends AppCompatActivity {
 Intent voltar;
@@ -33,5 +35,19 @@ Intent register;
                 startActivity(register);
             }
         });
+
+        ImageButton google = (ImageButton)findViewById(R.id.google);
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&prompt=consent&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&response_type=code&client_id=817455743730-8aptanrqdh06q6aje2jhdp7i30l38mo8.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Ffcm-tour.herokuapp.com%2Flogin"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
