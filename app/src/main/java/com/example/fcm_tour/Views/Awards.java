73bbc7d5506b19a3317aad40a2148ee88a122fa1
RuiFilipeSilva <rfcs23@gmail.com>
2021-01-16 19:06:17 +0000

    Awards

--- app/src/main/java/com/example/fcm_tour/Views/Awards.java
+++ app/src/main/java/com/example/fcm_tour/Views/Awards.java
@@ -1,5 +1,6 @@
 package com.example.fcm_tour.Views;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -7,12 +8,16 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -37,6 +42,8 @@ public class Awards extends Fragment {
     private static String[] names, prices, imgs, numbers;
     String name, price, img;
     Bundle extras;
+    ImageButton comeback;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,8 +55,86 @@ public class Awards extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_awards, container, false);
+        extras = new Bundle();
         new GetAwards().execute(API.API_URL + "/roleta/premios");
+        comeback = (ImageButton)v.findViewById(R.id.back);
+        comeback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int homeContainer = R.id.fullpage;
+                Roullete roullete = new Roullete();
+                backFragment(roullete, homeContainer);
+            }
+        });
+
         return v;
+
+    }
+
+    private void locationSort(JSONArray result) throws JSONException {
+        View v = getView();
+        ListView listView = v.findViewById(R.id.listAwards);
+        names = new String[result.length()];
+        prices = new String[result.length()];
+        imgs = new String[result.length()];
+        numbers = new String[result.length()];
+        for (int i = 0; i <= result.length() - 1; i++) {
+            JSONObject room = result.getJSONObject(i);
+            String name = room.getString("name");
+            String price = room.getString("price");
+            String img = room.getString("img");
+            String number = room.getString("number");
+            names[i] = name;
+            prices[i] = price;
+            imgs[i] = img;
+            numbers[i] = number;
+        }
+        MyAdapter adapter = new MyAdapter(getContext(), names, prices, imgs, numbers);
+        listView.setAdapter(adapter);
+        Log.d("SIGA", "locationSort: "+ numbers);
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            for (int i = 0; i < numbers.length; i++) {
+                if (position == i) {
+                    Log.d("SIGA", "locationSort: " + numbers[i]);
+                    new GetAwardsByNumber().execute(API.API_URL + "/roleta/premios/" + numbers[i]);
+                    Log.d("SIGA", "locationSort: " + API.API_URL + "/roleta/premios/" + numbers[i]);
+                    break;
+                }
+            }
+        });
+    }
+
+    static class MyAdapter extends ArrayAdapter<String> {
+        Context context;
+        String aName[];
+        String aPrice[];
+        String aImg[];
+        String aNumber[];
+
+        MyAdapter(Context c, String name[], String price[], String img[], String number[]) {
+            super(c, R.layout.award, R.id.name, name);
+            this.context = c;
+            this.aName = name;
+            this.aPrice = price;
+            this.aImg = img;
+            this.aNumber = number;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.award, parent, false);
+            TextView names = row.findViewById(R.id.name);
+            names.setText(aName[position]);
+            TextView cupertinos = row.findViewById(R.id.cupertinos);
+            cupertinos.setText(aPrice[position]);
+            ImageView imgs = row.findViewById(R.id.img);
+            Picasso.get()
+                    .load(aImg[position])
+                    .into(imgs);
+            return row;
+        }
     }
 
     class GetAwards extends AsyncTask<String, String, String> {
@@ -85,66 +170,7 @@ public class Awards extends Fragment {
         }
     }
 
-    private void locationSort(JSONArray result) throws JSONException {
-        View v = getView();
-        ListView listView = v.findViewById(R.id.listAwards);
-        names = new String[result.length()];
-        prices = new String[result.length()];
-        imgs = new String[result.length()];
-        numbers = new String[result.length()];
-        for (int i = 0; i <= result.length() - 1; i++) {
-            JSONObject room = result.getJSONObject(i);
-            String name = room.getString("name");
-            String price = room.getString("price");
-            String img = room.getString("img");
-            String number = room.getString("number");
-            names[i] = name;
-            prices[i] = price;
-            imgs[i] = img;
-        }
-        MyAdapter adapter = new MyAdapter(getContext(), names, prices, imgs, numbers);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            for (int i = 0; i < numbers.length; i++) {
-                        Preferences.removeRoom();
-                        Preferences.write("number", numbers[i]);
-                        new GetAwardsByNumber().execute(API.API_URL + "/torre/salas/" + numbers[i]);
-                        break;
-            }
-        });
 
-    }
-
-    static class MyAdapter extends ArrayAdapter<String> {
-        Context context;
-        String aName[];
-        String aPrice[];
-        String aImg[];
-        String aNumber[];
-
-        MyAdapter(Context c, String name[], String price[], String img[], String number[]) {
-            super(c, R.layout.room, R.id.name, name);
-            this.context = c;
-            this.aName = name;
-            this.aPrice = price;
-            this.aImg = img;
-            this.aNumber = number;
-        }
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View row = layoutInflater.inflate(R.layout.award, parent, false);
-            TextView names = row.findViewById(R.id.name);
-            names.setText(aName[position]);
-            TextView cupertinos = row.findViewById(R.id.cupertinos);
-            cupertinos.setText(aPrice[position]);
-            ImageView imgs = row.findViewById(R.id.img);
-            Log.d("SIGA", "getView: " + aImg[position]);
-            Picasso.get()
-                    .load(aImg[position])
-                    .into(imgs);
-            return row;
-        }
-    }
 
     class GetAwardsByNumber extends AsyncTask<String, String, String> {
         @Override
@@ -170,20 +196,38 @@ public class Awards extends Fragment {
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
             try {
-                JSONObject rooms = new JSONObject(result);
-                name = rooms.getString("name");
-                price = rooms.getString("prices");
-                img = rooms.getString("img");
+                JSONObject awards = new JSONObject(result);
+                name = awards.getString("name");
+                price = awards.getString("price");
+                img = awards.getString("img");
                 extras.putString("name", name);
                 extras.putString("price", price);
                 extras.putString("img", img);
-                /*final int homeContainer = R.id.fullpage;
-                AudioPage audioPage = new AudioPage();
-                audioPage.setArguments(extras);
-                openFragment(audioPage, homeContainer);*/
+                final int homeContainer = R.id.fullpage;
+                AwardsPage awardsPage = new AwardsPage();
+                awardsPage.setArguments(extras);
+                openFragment(awardsPage, homeContainer);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
+        private void openFragment(AwardsPage awardsPage, int homeContainer) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            ft.setCustomAnimations(R.anim.from_left, R.anim.to_right);
+            ft.addToBackStack(null);
+            ft.replace(homeContainer, awardsPage);
+            ft.commit();
+        }
+
+
+    }
+    private void backFragment(Roullete roullete, int homeContainer) {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.setCustomAnimations(R.anim.from_right, R.anim.to_left);
+        ft.addToBackStack(null);
+        ft.replace(homeContainer, roullete);
+        ft.commit();
     }
 }
---
+++ app/src/main/java/com/example/fcm_tour/Views/AwardsPage.java
@@ -0,0 +1,150 @@
+package com.example.fcm_tour.Views;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fcm_tour.R;
+import com.google.android.material.textfield.TextInputLayout;
+import com.squareup.picasso.Picasso;
+
+
+public class AwardsPage extends Fragment {
+    Bundle extras, bundle;
+    String name, price, imgs;
+    ImageButton comeback;
+    EditText nameClient, adress, postalCode, city;
+    LinearLayout form;
+    Button getAward;
+    RelativeLayout alert;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bundle = this.getArguments();
+        extras = new Bundle();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =inflater.inflate(R.layout.fragment_awards_page, container, false);
+        name = bundle.getString("name");
+        Log.d("SIGA", "onCreateView: " + name);
+        TextView text = (TextView)v.findViewById(R.id.name);
+        text.setText(name);
+        price = bundle.getString("price");
+        TextView prices = (TextView)v.findViewById(R.id.cupertinos);
+        prices.setText(price);
+        int cupertinos = Integer.parseInt(price);
+        imgs = bundle.getString("img");
+        ImageView img = (ImageView)v.findViewById(R.id.img);
+        Picasso.get().load(imgs).into(img);
+        comeback = (ImageButton)v.findViewById(R.id.back);
+
+        form = v.findViewById(R.id.form);
+        alert = v.findViewById(R.id.alert);
+        nameClient = (EditText)v.findViewById(R.id.nametxt);
+        adress = (EditText)v.findViewById(R.id.adressTxt);
+        postalCode = (EditText)v.findViewById(R.id.postalCodeTxt);
+        city = (EditText)v.findViewById(R.id.cityTxt);
+        getAward = (Button)v.findViewById(R.id.getAward);
+
+        comeback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int homeContainer = R.id.fullpage;
+                Awards awards = new Awards();
+                backFragment(awards, homeContainer);
+            }
+        });
+
+        if(cupertinos<1000){
+            form.setVisibility(View.VISIBLE);
+            alert.setVisibility(View.INVISIBLE);
+            getAward.setText(R.string.get_awards);
+            getAward.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("SIGA", "onClick: AQUI" );
+                    if(nameClient.getText().toString().equals("") || adress.getText().toString().equals("") || postalCode.getText().toString().equals("") || city.getText().toString().equals("")){
+                        AlertDialog alertDialog = new AlertDialog.Builder(getContext()).create();
+                        alertDialog.setTitle(R.string.title_get_awards);
+                        alertDialog.setMessage(getString(R.string.message_get_award));
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                (dialog, which) -> {
+                                    dialog.dismiss();
+                                });
+                        alertDialog.show();
+                    }
+                    else{
+                        AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                        alert.setTitle(R.string.conf_title_get_awards);
+                        alert.setMessage(getString(R.string.conf_message_get_awards));
+                        alert.setPositiveButton("Confirmar", (dialog, whichButton) -> {
+                            dialog.dismiss();
+                            final int homeContainer = R.id.fullpage;
+                            Awards awards = new Awards();
+                            backFragment(awards, homeContainer);
+                            Toast.makeText(getContext(), R.string.toast_get_awards, Toast.LENGTH_LONG).show();
+                        });
+                        alert.setNegativeButton("Cancelar",
+                                (dialog, which) -> {
+                                    dialog.dismiss();
+                                    return;
+                                });
+
+                        alert.show();
+                    }
+
+                }
+            });
+        }
+        else{
+            form.setVisibility(View.INVISIBLE);
+            getAward.setText(R.string.back);
+            alert.setVisibility(View.VISIBLE);
+            getAward.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final int homeContainer = R.id.fullpage;
+                    Awards awards = new Awards();
+                    backFragment(awards, homeContainer);
+
+                }
+            });
+
+        }
+
+
+        return v;
+    }
+
+    private void backFragment(Awards awards, int homeContainer) {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.setCustomAnimations(R.anim.from_right, R.anim.to_left);
+        ft.addToBackStack(null);
+        ft.replace(homeContainer, awards);
+        ft.commit();
+    }
+}
